e09669a70159fa4da6a866b95e3bbbd5d695c454
Dakota Clark <dakotaclark92@yahoo.com>
2019-08-08 01:44:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@
           item_price = req.path.split("/items").last
           item = @@item.find{|i| i.price == item_price}
           
-          resp.write item.name
+          resp.write item.price
         else
           resp.write "Route not found"
           resp.status = 404
