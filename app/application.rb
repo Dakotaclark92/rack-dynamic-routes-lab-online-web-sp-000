d5f0fee790efe90bb949db3a5a35bb4883dd4257
Dakota Clark <dakotaclark92@yahoo.com>
2019-08-08 01:28:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,26 +8,12 @@
         
         if req.path.match(/items/)
           
-          item_
-    
-    
-        class Application
-     
-      @@songs = [Song.new("Sorry", "Justin Bieber"),
-                Song.new("Hello","Adele")]
-     
-      def call(env)
-        resp = Rack::Response.new
-        req = Rack::Request.new(env)
-     
-        if req.path.match(/songs/)
-     
-          song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
-          song = @@songs.find{|s| s.title == song_title}
-     
-          resp.write song.artist
+          item_name = req.path.split("/items").last
+          item = @@items.find{|i| i.name == item_name}
+          
+          resp.write item.price
         end
-     
+        
         resp.finish
       end
     end
